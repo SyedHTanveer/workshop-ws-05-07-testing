f13b8a99812e70ebdaf439ce88f5903d65946e03
SyedHTanveer <Syedt21@gmail.com>
2019-05-07 15:18:34 -0400

    workshop complete.

--- src/youtube-api.js
+++ src/youtube-api.js
@@ -1,7 +1,27 @@
+/* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
+const STATISTICS_API_URL = 'https://www.googleapis.com/youtube/v3/videos';
 const API_URL = 'https://www.googleapis.com/youtube/v3/search';
-const API_KEY = 'YOUR_KEY';
+const API_KEY = 'AIzaSyC-0Ecu9iiw1ew4MUzQ5r_b_UuYbJX90Rk';
+
+export const viewCountByVideo = (videoId) => {
+  const params = {
+    key: API_KEY,
+    id: videoId,
+    part: 'statistics',
+  };
+  return new Promise((resolve, reject) => {
+    axios.get(STATISTICS_API_URL, { params })
+      .then((response) => {
+        resolve(Number(response.data.items[0].statistics.viewCount));
+      })
+      .catch((error) => {
+        console.log(`youtube statistics api error: ${error}`);
+        reject(error);
+      });
+  });
+};
 
 export const youtubeSearch = (term) => {
   const params = {
